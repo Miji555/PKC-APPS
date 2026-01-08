83bab9d7f570e6d6dfa68b9d154d69aa82b5a7f7
Miji555 <147109321+Miji555@users.noreply.github.com>
2026-01-09 05:52:00 +0700

    feat: Add Garena RoV app to list
    Includes the RoV app details, name, subtitle, URL, and icon to the application list.

--- constants.ts
+++ constants.ts
@@ -12,6 +12,13 @@ export const CATEGORIES: Category[] = [
         subtitle: 'เกมจำลองการสร้างบล็อก',
         url: 'https://apps.apple.com/th/app/minecraft/id479516143',
         icon: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/91/9e/7b/919e7b30-c831-50e5-7973-206e0176f183/AppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/100x100bb.png'
+      },
+      {
+        id: 'rov',
+        name: 'Garena RoV: 5v5 Festival',
+        subtitle: 'สุดยอดเกม MOBA 5v5 บนมือถือ',
+        url: 'https://apps.apple.com/th/app/garena-rov-5v5-festival/id1150337432',
+        icon: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/21/92/8a/21928ab1-2a6c-e06a-9351-831518f9746b/AppIcon-0-0-1x_U007emarketing-0-10-0-85-220.png/100x100bb.png'
       }
     ]
   },
